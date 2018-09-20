cb3a320d33aa744acfd9b000379f68b4c38f2cd0
info-smart-lands <40959733+info-smart-lands@users.noreply.github.com>
2018-09-20 22:50:43 +0800

    Create smla_info.py

---
+++ smla_info/smla_info.py
@@ -0,0 +1,26 @@
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 09 20 15:50:20 2018
+
+@project: smla_info
+@author : likw
+@company: HuMan Ltd.,Co.
+"""
+
+def hi():
+    print(":) Ni Hao!")
+
+
+def hello():
+    print(":) Ni Hao! Welcome to Smart Lands.")
+
+
+def about():
+    print(":) A machine intelligence company focused on natural language understanding.")
+
+
+def contact():
+    print(":) Email: info@smart-lands.com")
+
